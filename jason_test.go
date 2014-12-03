ccd82bfb6b577b63d3d234e8eb41f9895039657f
Anton Holmquist <anton.holmquist@gmail.com>
2014-12-03 23:38:23 +0100

    Updated tests

--- jason_test.go
+++ jason_test.go
@@ -59,7 +59,7 @@ func TestFirst(t *testing.T) {
 	assert.True(err == nil, "failed to create json from string")
 
 	s, err := j.Get("name").AsString()
-	assert.True(s == "anton" && err == nil, "name should be a string")
+	assert.True(s.String() == "anton" && err == nil, "name should be a string")
 	//assert.True(j.Get("name").IsObject() == false, "name should not be an object")
 
 	assert.True(j.object().Valid, "the object should be valid")
@@ -68,7 +68,7 @@ func TestFirst(t *testing.T) {
 	//assert.True(j.Has("name2") == false, "do not have name2")
 
 	s, err = j.Get("name").AsString()
-	assert.True(s == "anton" && err == nil, "name shoud match")
+	assert.True(s.String() == "anton" && err == nil, "name shoud match")
 
 	_, err = j.Get("age").AsNumber()
 	assert.True(err == nil, "age should be a number")
@@ -87,13 +87,13 @@ func TestFirst(t *testing.T) {
 	assert.True(address != nil && err == nil, "address should be an object")
 
 	addressAsString, err := j.Get("address").AsString()
-	assert.True(addressAsString == "" && err != nil, "address should not be an string")
+	assert.True(addressAsString.String() == "" && err != nil, "address should not be an string")
 
 	s, err = j.Get("address", "street").AsString()
-	assert.True(s == "Street 42" && err == nil, "street mismatching")
+	assert.True(s.String() == "Street 42" && err == nil, "street mismatching")
 
 	s, err = j.Get("address", "name2").AsString()
-	assert.True(s == "" && err != nil, "nonexistent string fail")
+	assert.True(s.String() == "" && err != nil, "nonexistent string fail")
 
 	assert.True(j.Get("address", "street").Exists() == true, "street shoud exist")
 	assert.True(j.Get("address", "street2").Exists() == false, "street should not exist")
@@ -121,16 +121,16 @@ func TestFirst(t *testing.T) {
 		//assert.True(element.IsObject() == true, "first fail")
 
 		s, err = element.Get("street").AsString()
-		assert.True(s == "Street 42" && err == nil, "second fail")
+		assert.True(s.String() == "Street 42" && err == nil, "second fail")
 	}
 
 	obj, err := j.Get("country").AsObject()
 	assert.True(obj != nil && err == nil, "country should not return error on AsObject")
-	for key, value := range obj {
+	for key, value := range obj.Map {
 
 		assert.True(key == "name", "country name key incorrect")
 
 		s, err = value.AsString()
-		assert.True(s == "Sweden" && err == nil, "country name should be Sweden")
+		assert.True(s.String() == "Sweden" && err == nil, "country name should be Sweden")
 	}
 }
